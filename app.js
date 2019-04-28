1c7d5ddd79227b63f475f04da38c36906f64dee1
hugolevino <50059470+hugolevino@users.noreply.github.com>
2019-04-27 21:35:35 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 const express = require('express');
+const rp = require('request-promise');
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send('WHAT?... SAY WHAT AGAIN!')
-    .end();
+
+	var options = {
+		method: 'GET',
+		uri: 'https://b2w-region-v1.b2w.io/b2w-region/region?zipcode=13467500',
+		resolveWithFullResponse: true,
+		headers: {
+			"content-type": "application/json",
+			"accept": "application/json"
+		},
+		json: true
+	};
+	 
+	rp(options).then(function (repos) {
+		 res.status(200);
+	     res.send(repos.body);
+	     res.end();
+	})
+	.catch(function (response) {
+		res.status(400);
+	    res.send('DEU XABU');
+	    res.end();
+	});
+
 });
 
-// Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
