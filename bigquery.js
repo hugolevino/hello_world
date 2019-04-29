d2339e3bc28626be63924db2c4889458b147dc25
hugolevino <50059470+hugolevino@users.noreply.github.com>
2019-04-28 21:23:33 -0300

    Update bigquery.js

--- bigquery.js
+++ bigquery.js
@@ -1,4 +1,16 @@
 const {BigQuery} = require('@google-cloud/bigquery');
+const cloudTasks = require('@google-cloud/tasks');
+const client = new cloudTasks.CloudTasksClient();
+const parent = client.queuePath('bigdata-bernard', 'us-central1', 'first-queue');
+
+const task = {
+	appEngineHttpRequest: {
+    	httpMethod: 'POST',
+    	relativeUri: '/listening',
+	},
+};
+
+
 var count_row = 0;
 var rows = '';
 
@@ -8,9 +20,9 @@ async function query() {
 
   const bigqueryClient = new BigQuery();
 
-  const query = `SELECT Name
-    FROM \`bigdata-bernard.my_new_dataset.my_new_table\`
-    LIMIT 821`;
+  const query = `SELECT cep
+    FROM \`bigdata-bernard.my_new_dataset.ceps\`
+    LIMIT 5`;
   const options = {
     query: query,
     location: 'US',
@@ -25,11 +37,25 @@ async function query() {
   
 }
 
-function testing(){
+async function testing(){
 	if(count_row < rows[0].length){
 		for (i = 0; i < 80; i++) {
 			if(count_row < rows[0].length){
-				console.log(count_row + ' -> ' + rows[0][i].Name);
+				
+				var payload = rows[0][count_row].cep;
+				task.appEngineHttpRequest.body = Buffer.from(payload).toString('base64');
+				const request = {
+				    parent: parent,
+				    task: task,
+				 };
+
+				 console.log('Sending task:');
+				 console.log(task);
+
+				 const [response] = await client.createTask(request);
+				 const name = response.name;
+				 console.log(`Created task ${name}`);
+
 				count_row++;
 			}
 		}
