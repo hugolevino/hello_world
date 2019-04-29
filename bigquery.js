6e05e77667b0989670a7f882856d687b707fd768
hugolevino <50059470+hugolevino@users.noreply.github.com>
2019-04-28 21:03:22 -0300

    Update bigquery.js

--- bigquery.js
+++ bigquery.js
@@ -1,4 +1,6 @@
 const {BigQuery} = require('@google-cloud/bigquery');
+var count_row = 0;
+var rows = '';
 
 query();
 
@@ -8,7 +10,7 @@ async function query() {
 
   const query = `SELECT Name
     FROM \`bigdata-bernard.my_new_dataset.my_new_table\`
-    LIMIT 5`;
+    LIMIT 821`;
   const options = {
     query: query,
     location: 'US',
@@ -17,9 +19,22 @@ async function query() {
   const [job] = await bigqueryClient.createQueryJob(options);
   console.log(`Job ${job.id} started.`);
 
-  const [rows] = await job.getQueryResults();
+  rows = await job.getQueryResults();
 
-  console.log('Rows:');
-  rows.forEach(row => console.log(row));
+  testing();
   
+}
+
+function testing(){
+	if(count_row < rows[0].length){
+		for (i = 0; i < 80; i++) {
+			if(count_row < rows[0].length){
+				console.log(count_row + ' -> ' + rows[0][i].Name);
+				count_row++;
+			}
+		}
+		setTimeout(function(){
+			 testing();
+		}, 5000);
+	}
 }
